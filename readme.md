34eb693cab9f00f0bd7877cd9bbf9d1b1da385a5
Nithin Meppurathu <nithin@nitmedia.com>
2019-07-09 22:38:18 -0400

    first commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Example local server with traefik
+
+* run setup_vhosts.sh
+* docker-compose -d up // to start traefik
+* docker-compose -f docker-compose.example_site.yml up // to create the demo sites to register into traefik
---
+++ setup_vhosts.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo bash -c 'echo "127.0.0.1 app.test" >> /etc/hosts'
+sudo bash -c 'echo "127.0.0.1 app1.test" >> /etc/hosts'
+sudo bash -c 'echo "127.0.0.1 app2.test" >> /etc/hosts'
+sudo bash -c 'echo "127.0.0.1 app3.test" >> /etc/hosts'
